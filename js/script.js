711272e10c559bf60f2196eb531bd5b2bc13766d
Gleb <glebsuper3@mail.ru>
2022-01-07 20:30:39 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
 
-
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};

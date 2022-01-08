8067c507a97097f2ceea06e552a322afa002d94a
Gleb <glebsuper3@mail.ru>
2022-01-09 00:34:58 +0500

    five commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,12 +9,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько вы оцените его?', ""),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько вы оцените его?', "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+          b = prompt('На сколько вы оцените его?', "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }else{
+        console.log('error');
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+}else if(personalMovieDB.count > 10 &&  personalMovieDB.count < 30){
+    alert("Вы классический зритель");
+}else if(personalMovieDB.count > 30){
+    alert('Вы киноман!');
+}else{
+    alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
